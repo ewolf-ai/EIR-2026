761dbcf78d357fd69d309a3a1aad63f9202ab49a
roberottt <robertoot99@gmail.com>
2026-03-08 14:40:34 +0100

    Fixed total registered users in global table

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import AuthProvider from '@/components/AuthProvider';
 
@@ -24,12 +25,30 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="monetag" content="774ad1aa30513e6183591da0c343f1e9" />
-        <script src="https://5gvci.com/act/files/tag.min.js?z=10691679" data-cfasync="false" async></script>
-        <script dangerouslySetInnerHTML={{__html: `(function(s){s.dataset.zone='10691682',s.src='https://gizokraijaw.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}} />
-        <script dangerouslySetInnerHTML={{__html: `(function(s){s.dataset.zone='10691683',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}} />
       </head>
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
+        
+        {/* Scripts moved after body to prevent hydration errors */}
+        <Script 
+          src="https://5gvci.com/act/files/tag.min.js?z=10691679" 
+          data-cfasync="false" 
+          strategy="afterInteractive"
+        />
+        <Script 
+          id="vignette-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(s){s.dataset.zone='10691682',s.src='https://gizokraijaw.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+          }}
+        />
+        <Script 
+          id="tag-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(s){s.dataset.zone='10691683',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+          }}
+        />
       </body>
     </html>
   );
